c3e1f9a5c4f65c7a6db64ad61ac6898afa08c077
Jayy1950 <114063180+Jayy1950@users.noreply.github.com>
2022-10-02 09:39:17 +0530

    Add files via upload

---
+++ Example1.java
@@ -0,0 +1,20 @@
+interface PrivateMethod{
+    private String reverseString(String string){
+        StringBuffer sb = new StringBuffer(string);
+        return sb.reverse().toString();
+    }
+    static String toUpperCase(String s){
+        return s.toUpperCase();
+    }
+
+    static String toLowerCase(String s){
+        return s.toLowerCase();
+    }
+}
+
+public class Example1 {
+    public static void main(String[] args) {
+        System.out.println(PrivateMethod.toLowerCase("My name is Jay Yadav"));
+        System.out.println(PrivateMethod.toLowerCase("HELLO THERE"));
+    }
+}
